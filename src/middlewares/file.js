267c5bc49767d9e18c08f869a2edc3e739aeb36f
GonzalokHM <gonzalohm93@gmail.com>
2024-05-28 14:36:15 +0200

    fix sintax

--- src/middlewares/file.js
+++ src/middlewares/file.js
@@ -5,7 +5,7 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'fullorg',
+    folder: 'fullBack1',
     allowedFormats: ['jpg', 'jpeg', 'png', 'gif', 'webp'],
   },
 });
