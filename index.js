b324785fe3525bfcff88489dde3e6aa51371c441
GonzalokHM <gonzalohm93@gmail.com>
2024-06-27 07:19:06 +0200

    fix cors config

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connectDB();
 
 // Configurar CORS con opciones específicas
 const corsOptions = {
-  origin: 'eventstorge.netlify.app',
+  origin: 'https://eventstorge.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
@@ -22,7 +22,7 @@ app.options('*', cors(corsOptions));
 
 app.use((req, res, next) => {
     console.log(`${req.method} request to ${req.url}`);
-    res.header('Access-Control-Allow-Origin', 'eventstorge.netlify.app');
+    res.header('Access-Control-Allow-Origin', 'https://eventstorge.netlify.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
