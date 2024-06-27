cd9b154fcad4abfcec19bf46ae7816d6ab344188
GonzalokHM <gonzalohm93@gmail.com>
2024-06-27 10:43:34 +0200

    fix corsh

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.options('*', cors(corsOptions));
 
 app.use((req, res, next) => {
     console.log(`${req.method} request to ${req.url}`);
-    res.header('Access-Control-Allow-Origin', 'https://eventstorge.netlify.app');
+    // res.header('Access-Control-Allow-Origin', 'https://eventstorge.netlify.app');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
