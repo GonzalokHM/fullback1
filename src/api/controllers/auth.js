f1c703a9a571b6a6725a0310d084bfbaf9c4ef0f
GonzalokHM <gonzalohm93@gmail.com>
2024-06-27 08:52:56 +0200

    fix errors auth

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -9,12 +9,14 @@ const register = async (req, res, next) => {
     const userDuplicate = await User.findOne({ name: req.body.name });
 
     if (userDuplicate) {
-      return next(setError(400, 'this user alredy exists 😱'));
+      // return next(setError(400, 'this user alredy exists 😱'));
+      return res.status(400).json({ message: 'This user already exists 😱' });
     }
     const user = await newUser.save();
     return res.status(201).json(user);
   } catch (error) {
-    return next(setError(400, "can't register 😞"));
+    // return next(setError(400, "can't register 😞"));
+    return res.status(400).json({ message: "Can't register 😞" });
   }
 };
 
@@ -25,17 +27,20 @@ const login = async (req, res, next) => {
       { email: req.body.email }
     ]});
     if (!user) {
-      return next(setError(400, "this doesn't exist 😞"));
+      // return next(setError(400, "this doesn't exist 😞"));
+      return res.status(400).json({ message: "This user doesn't exist 😞" });
     }
 
     if (bcrypt.compareSync(req.body.password, user.password)) {
       const token = generateSign(user._id);
       return res.status(200).json({ user, token });
     } else {
-      return next(setError(400, "passwords don't macth 😞"));
+      // return next(setError(400, "passwords don't macth 😞"));
+      return res.status(400).json({ message: "Passwords don't match 😞" });
     }
   } catch (error) {
-    return next(setError(400, "can't login 😞"));
+    // return next(setError(400, "can't login 😞"));
+    return res.status(400).json({ message: "Can't login 😞" });
   }
 };
 
